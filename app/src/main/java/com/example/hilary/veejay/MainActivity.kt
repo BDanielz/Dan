4b958edcbb75564d10824d636446a69ac0581aee
safariH <safarihilary9@gmail.com>
2017-10-20 20:30:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hilary/veejay/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hilary.veejay;
+
+
+        import android.support.v7.app.AppCompatActivity
+        import android.os.Bundle
+        import android.widget.Button
+        import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val btn_click_me = findViewById(R.id.btn_click_me) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+        // your code to perform when the user clicks on the button
+        Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+        }
+
+
